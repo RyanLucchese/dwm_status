45531188f0e627ed3979c16baceafad9c3aa5448
Ryan Lucchese <ryan.lucchese@write-c.com>
2014-12-26 20:39:48 -0700

    added cpu usage calculation

--- src/dwm_status.c
+++ src/dwm_status.c
@@ -15,12 +15,28 @@
  *
  */
 
+// This software is intended for use on FreeBSD. It is unlikely that it will work on other systems.
+
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include <sys/param.h>
+#include <sys/resource.h>
 #include <sys/sysctl.h>
+#include <unistd.h>
 #include <X11/Xlib.h>
 
+// SIGINT handler to break out of main loop
+static bool done = false;
+void sigint_handler(int unused)
+{
+	(void)unused;
+	done = true;
+}
+
+// sysctl -n hw.ncpu
 size_t get_number_of_cpus()
 {
 	int hw_ncpu;
@@ -32,6 +48,33 @@ size_t get_number_of_cpus()
 	}
 
 	return (size_t)(hw_ncpu);
+}
+
+// sysctl -n kern.cp_times
+void get_cp_times(long* pcpu, size_t count)
+{
+	size_t pcpu_size = sizeof(*pcpu) * count;
+	if (sysctlbyname("kern.cp_times", pcpu, &pcpu_size, NULL, 0) < 0)
+	{
+		return;
+	}
+}
+
+// returns percent usage: 100% - idle%
+double cpu_usage(long* current, long* previous)
+{
+	// find the difference of total ticks in all cpu states
+	long total = 0;
+	for (size_t i = 0; i < CPUSTATES; i++)
+	{
+		long prev = current[i];
+		current[i] -= previous[i];
+		previous[i] = prev;
+		total += current[i];
+	}
+
+	// the usage is 100% minus idle%
+	return (100L - (100L * current[CPUSTATES - 1] / (total ? total : 1L)));
 }
 
 int main()
@@ -46,11 +89,46 @@ int main()
 
 	// get cpu count
 	const size_t hw_ncpu = get_number_of_cpus();
-	(void)hw_ncpu;
+
+	// allocate pcpu buffers
+	const size_t pcpu_count = hw_ncpu * CPUSTATES;
+	long* pcpu_current = calloc(sizeof(long), pcpu_count);
+	long* pcpu_previous = calloc(sizeof(long), pcpu_count);
+
+	// set up intitial values
+	get_cp_times(pcpu_current, pcpu_count);
+
+	// update status
+	signal(SIGINT, sigint_handler);
+	const size_t status_size = 256;
+	char status[status_size] = { 0 };
+	while (!done)
+	{
+		// update cpu times
+		get_cp_times(pcpu_current, pcpu_count);
+
+		// prepare status message
+		size_t offset = snprintf(status, status_size, "[ CPU ");
+		for (size_t i = 0; i < hw_ncpu; i++)
+		{
+			offset += snprintf(status + offset, status_size - offset, "%.0f%% ", cpu_usage(&pcpu_current[i * CPUSTATES], &pcpu_previous[i * CPUSTATES]));
+		}
+		offset += snprintf(status + offset, status_size - offset, "]");
+
+		// update root window name
+		XStoreName(display, DefaultRootWindow(display), status);
+		XSync(display, False);
+
+		// sleep for 1 second
+		sleep(1);
+	}
 
 	// clean up
+	free(pcpu_current);
+	free(pcpu_previous);
 	XCloseDisplay(display);
 
+	// all done
 	return 0;
 }
 
